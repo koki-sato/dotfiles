bbd3225a1b21456fcfeb8ce4e0cd5281c9014a36
Koki Sato <s.4115.koki@gmail.com>
2017-10-24 12:00:00 +0900

    add bin/dotfiles and update install.sh

--- install.sh
+++ install.sh
@@ -2,16 +2,51 @@
 
 set -eu
 
-tempfile=/tmp/dotfiles.zip
-workspace=/tmp/dotfiles
+export DOTFILES_ROOT="$HOME/dotfiles"
 
-# Download zipped installer
-curl -LSfs -o ${tempfile} https://github.com/koki-sato/dotfiles/archive/master.zip
+#===========================================================================#
+# common functions
+#===========================================================================#
 
-# Unzip installer into workspace
-unzip -oq ${tempfile} -d ${workspace}
+print() {
+  printf "\e[4mdotfiles\e[0m: "
+  echo "$@"
+}
 
-cp -r ${workspace}/dotfiles-master/home $HOME
+cmd_exec() {
+  print "$@"
+  "$@"
+}
 
-# Cleanup
-rm -rf ${tempfile} ${workspace}
+command_exist() {
+  which "$1" >/dev/null 2>&1
+  return $?
+}
+
+#===========================================================================#
+
+if [[ -d "$DOTFILES_ROOT" ]]; then
+  print "Dotfiles is already installed. Override it."
+  cmd_exec rm -fr $DOTFILES_ROOT
+  print
+fi
+
+if ! command_exist git; then
+  print "Error: Dotfiles need git."
+  exit 1
+fi
+
+cmd_exec git clone --recursive https://github.com/koki-sato/dotfiles.git "$DOTFILES_ROOT"
+cmd_exec cd $DOTFILES_ROOT
+print
+
+case $OSTYPE in
+  darwin*)
+    cmd_exec $DOTFILES_ROOT/bin/dotfiles setup-osx
+    ;;
+  linux*)
+    cmd_exec $DOTFILES_ROOT/bin/dotfiles setup-linux
+    ;;
+esac
+
+cmd_exec $DOTFILES_ROOT/bin/dotfiles link
